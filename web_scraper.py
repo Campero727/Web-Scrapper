825f2169f80f4cf14454f6fb5cfe722348736d9a
Campero727 <campero1008@gmail.com>
2022-04-03 23:04:53 -0500

    Add write option

--- web_scraper.py
+++ web_scraper.py
@@ -1,6 +1,10 @@
+import keyboard;
+from getpass import getuser;
+
 def main():
 	banner();
-	pass;
+	realizar_scrapping();
+	
 
 def banner():
 	print("           $           $");
@@ -29,6 +33,18 @@ def banner():
 	print("    $$    $    $$$$$    $    $$");
 	print("     $     $           $     $");
 	print("      $    $           $    $");
+
+def realizar_scrapping():
+	url=input("Ingresa la URL del sitio Web");
+	ruta=input("Ingresa la ruta para guardar los documentos");
+	nombre=input("Ingresa el nombre del documento");
+	try:
+		data=open(ruta+'data.txt','wb');
+		data.write(url.encode()+b'\n');
+		data.write(ruta.encode()+b'\n');
+		data.write(nombre.encode()+b'\n');
+	except:
+		print("No se han podido registrar los datos para continuar");
 
 if __name__=='__main__':
 	main();

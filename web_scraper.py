3d7c6967cf61e4c6075aec9c4c20d5c3ffb4e75e
Campero727 <campero1008@gmail.com>
2022-04-05 00:24:28 -0500

    Add Parameters

--- web_scraper.py
+++ web_scraper.py
@@ -1,9 +1,30 @@
 import keyboard;
 from getpass import getuser;
+import argparse;
+import urllib.request
 
 def main():
 	banner();
-	realizar_scrapping();
+	try:
+		parser = argparse.ArgumentParser(description='Manual to this script')
+		parser.add_argument('--url', type=str, default = None, help="Url para realizar el web Scrapping");
+		parser.add_argument('--dir',type=str, default=None,help="Directorio Para Guardar los archivos");
+		parser.add_argument('--fname',type=str, default=None, help="Nombre del archivo HTML y TXT");
+		parser.add_argument('--etiqueta', type=str,default=None, help="Etiqueta a buscar");
+		args = parser.parse_args()
+		if args.url and args.dir:
+			if args.fname:
+				if args.etiqueta:
+					web_scrapper(args.url,args.dir,args.fname,args.etiqueta);
+				else:
+					web_scrapper(args.url,args.dir,args.fname,"");
+			else:
+				web_scrapper(args.url,args.dir,'data',"");
+				print("Los documentos se guardaran en "+args.dir+" con el nombre data");
+		else:
+			print("Ingresa los valores para comenzar con el Web Scrapping");
+	except:
+		print("");
 	
 
 def banner():
@@ -34,17 +55,33 @@ def banner():
 	print("     $     $           $     $");
 	print("      $    $           $    $");
 
-def realizar_scrapping():
-	url=input("Ingresa la URL del sitio Web");
-	ruta=input("Ingresa la ruta para guardar los documentos");
-	nombre=input("Ingresa el nombre del documento");
-	try:
-		data=open(ruta+'data.txt','wb');
-		data.write(url.encode()+b'\n');
-		data.write(ruta.encode()+b'\n');
-		data.write(nombre.encode()+b'\n');
-	except:
-		print("No se han podido registrar los datos para continuar");
+def web_scrapper(url,directorio,fname,etiqueta):
+
+	dir_html=str(directorio+fname+'.html');
+	web=open(dir_html,'wb');
+	consulta=urllib.request.urlopen(url);
+	consulta=consulta.read();
+	web.write(consulta); 
+	web.close();
+	print("Pagina Clonada En "+directorio);
+	
+	if etiqueta:
+		dir_txt=str(directorio+fname+'.txt');
+		txt=open(dir_txt,'wb');
+		
+		files=open(dir_html,'rb');
+		inicio=str('<'+etiqueta+'>');
+		fin=str('</'+etiqueta+'>');
+
+		for l in files.readlines():
+			l=str(l);
+			if inicio in l:
+				x=l.find(inicio);
+				x=x+len(inicio);
+				y=l.find(fin);
+				#print(l[x:y]);
+				txt.write((l[x:y]+'\n').encode());
+		print("Documento de texto con el contenido de las etiqutas <"+etiqueta+'>'+' se encuentran en el directorio '+directorio);
 
 if __name__=='__main__':
 	main();
